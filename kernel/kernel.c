258a6257f3c744d85a635f717e2db820dd2012c4
Simone3838 <simone.paolini14@gmail.com>
2025-01-01 14:32:14 +0100

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,6 +1,7 @@
-// kernel/kernel.c - Basic kernel code with graphics
+// kernel/kernel.c - Basic kernel code with graphics and font rendering
 
 #include "graphics.h"
+#include "font.h"
 
 void kernel_main(void) {
     init_graphics();
@@ -8,8 +9,8 @@ void kernel_main(void) {
     // Clear screen by drawing a black rectangle over the entire screen
     draw_rect(0, 0, VGA_WIDTH, VGA_HEIGHT, 0);
 
-    // Draw a white rectangle
-    draw_rect(50, 50, 100, 50, 15);
+    // Draw a string "Hello, S OS!" at position (50, 50)
+    draw_string(50, 50, "Hello, S OS!", 15);
 
     // Infinite loop to keep the kernel running
     while (1) {
